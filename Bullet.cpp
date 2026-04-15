693254bd51bbb9aae4dba6c3eadac31b4963afa4
alexispros2008-lang <alexispros2008@outlook.com>
2026-04-14 20:30:01 -0400

    Ajout d'un constructeur et destructeur pour la class Bullet

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,1 +1,22 @@
 #include "Bullet.h"
+
+Bullet::Bullet()
+{
+	_positionBulletX = 0;
+	_positionBulletY = 0;
+	_bulletSpeed = 0;
+}
+
+Bullet::Bullet(int positionBulletX, int positionBulletY, int bulletSpeed)
+{
+	_positionBulletX = positionBulletX;
+	_positionBulletY = positionBulletY;
+	_bulletSpeed = bulletSpeed;
+}
+
+Bullet::~Bullet()
+{
+	_positionBulletX = 0;
+	_positionBulletY = 0;
+	_bulletSpeed = 0;
+}
--- Bullet.h
+++ Bullet.h
@@ -1,5 +1,13 @@
 #pragma once
 class Bullet
 {
+	protected:
+		int _positionBulletX;
+		int _positionBulletY;
+		int _bulletSpeed;
+	public:
+		Bullet();
+		Bullet(int positionBulletX, int positionBulletY, int bulletSpeed);
+		~Bullet();
 };
 
