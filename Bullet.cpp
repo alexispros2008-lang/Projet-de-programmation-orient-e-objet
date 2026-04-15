7a3802eb773fbfe50985deb179726bb960b8d72c
alexispros2008-lang <alexispros2008@outlook.com>
2026-04-15 16:23:07 -0400

    Ajouter _direction et _damageAmount dans Bullet

--- Bullet.cpp
+++ Bullet.cpp
@@ -5,13 +5,17 @@ Bullet::Bullet()
 	_positionBullet.x = 0;
 	_positionBullet.y = 0;
 	_bulletSpeed = 0;
+	_direction = NOTHING;
+	_damageAmount = 0;
 }
 
-Bullet::Bullet(sf::Vector2 <int> positionBullet, int bulletSpeed)
+Bullet::Bullet(sf::Vector2 <int> positionBullet, int bulletSpeed, int damageAmount, Direction direction)
 {
 	_positionBullet.x = positionBullet.x;
 	_positionBullet.y = positionBullet.y;
 	_bulletSpeed = bulletSpeed;
+	_direction = direction;
+	_damageAmount = damageAmount;
 }
 
 Bullet::~Bullet()
@@ -19,4 +23,6 @@ Bullet::~Bullet()
 	_positionBullet.x = 0;
 	_positionBullet.y = 0;
 	_bulletSpeed = 0;
+	_direction = NOTHING;
+	_damageAmount = 0;
 }
--- Bullet.h
+++ Bullet.h
@@ -1,13 +1,23 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+enum Direction {
+	LEFT,
+	IP,
+	DOWN,
+	RIGHT,
+	NOTHING
+};
+
 class Bullet
 {
 	protected:
 		sf::Vector2 <int> _positionBullet;
 		int _bulletSpeed;
+		int _damageAmount;
+		Direction _direction;
 	public:
 		Bullet();
-		Bullet(sf::Vector2 <int> positionBullet, int bulletSpeed);
+		Bullet(sf::Vector2 <int> positionBullet, int bulletSpeed, int damageAmount, Direction direction);
 		~Bullet();
 };
 
