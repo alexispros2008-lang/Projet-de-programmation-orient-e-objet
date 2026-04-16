d12f0bd5dbcfcb4eb4bb3d22e26fe23340dbe6e6
alexispros2008-lang <alexispros2008@outlook.com>
2026-04-16 11:41:03 -0400

    Updated pattern, added read()

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,1 +1,32 @@
 #include "Pattern.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+void Pattern::readPaternFile(std::string paternFileName)
+{
+
+	std::ifstream paternFile;
+	paternFile.open(paternFileName);
+
+	if (!paternFile) {
+		exit(1);
+	}
+
+	sf::Vector2 <float> positionBullet;
+	int bulletSpeed;
+	int damageAmount;
+	int direction;
+	int radius;
+	Direction tempDirectionPush;
+
+	/*while (paternFile >> positionBullet.x >> positionBullet.y >> bulletSpeed >> damageAmount >> direction >> radius) {
+		if (direction = 4) {
+
+		}
+		Snowball tempSnowball(positionBullet, bulletSpeed, damageAmount, direction, radius)
+		_pattern.push_back()
+	}*/
+
+	//paternFile.close();
+}
--- Pattern.h
+++ Pattern.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <vector>
+#include "Snowball.h"
 class Pattern
 {
+	private:
+		std::vector <Snowball> _pattern;
+	public:
+		Pattern();
+		~Pattern();
+		void readPaternFile(std::string paternFileName);
 };
 
