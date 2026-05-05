7c82ee63603e65823883c2c967d5ed80e3f8bedf
alexispros2008-lang <alexispros2008@outlook.com>
2026-05-05 11:16:41 -0400

    Update Snowball.h

--- Snowball.h
+++ Snowball.h
@@ -2,12 +2,9 @@
 #include "Bullet.h"
 class Snowball : public Bullet
 {
-	private:
-		sf::Rect <float> _snowballBounds = _bulletCircle.getGlobalBounds();
 	public:
 		Snowball(sf::CircleShape bulletCircle, int bulletSpeed, float angle, int radius, float rotation, float bulletLifeTime);
 		~Snowball();
-		sf::Rect <float> getSnowballBounds() const { return _snowballBounds; }
 		const sf::CircleShape& getSnowballCircle();
 		void setColorWhite();
 		void setColorBlue();
